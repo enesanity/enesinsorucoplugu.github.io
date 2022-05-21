970ba39dfab6b953052ec8c5d7ca839ca16d5563
Enes Efe GİRGİN <88033491+enesanity@users.noreply.github.com>
2022-05-21 18:28:44 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+const header2 = document.querySelector("#header2");
+
+
+function soruyok() {
+    document.getElementById('header2').style.display = 'flex'
+    setTimeout(
+        function(){
+            document.getElementById('header2').style.display = 'none'
+        }
+    , 2000);
+}
+
+
---
+++ slider.js
@@ -0,0 +1,12 @@
+const resim = document.querySelector("#resim");
+
+
+function sorudegistirx() {
+    window.open('https://enesinsorucoplugu.github.io/mat1.png')
+}
+
+function sorudegistiry() {
+  window.open('https://enesinsorucoplugu.github.io/mat2.png')
+}
+
+
